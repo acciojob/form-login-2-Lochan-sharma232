6cc96c4aa00114db9a41a8c36dc623820a0d6462
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-11 22:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-let form = document.getElementById('myform');
-form.addEventListener('submit')
+document.addEventListener('DOMContentLoaded',function() {
+document.getElementById('myform').addEventListener('submit',function(event){
 //your JS code here. If required.
+event.preventDefault();
+});
