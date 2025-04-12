02350fec07441783ef50e8119eee14a4b4c84302
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-12 15:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.getElementById('myform').addEventListener('submit',function(event){
 event.preventDefault();
 const firstName=document.getElementById('firstName').value;
 const lastName=document.getElementById('lastName').value;
-const phoneNumber=document.getElementById('phoneName').value;
-const firstName=document.getElementById('emailID').value;
+const phoneNumber=document.getElementById('phoneNumber').value;
+const emailID=document.getElementById('emailID').value;
 	
 const alertmg=`First Name:${firstName}\n Last Name:${lastName}\nPhone Number${phonenumber}\n emailID${EmailID}`
 		alert(alertmg);
