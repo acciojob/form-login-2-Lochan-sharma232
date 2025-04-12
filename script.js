574c7dee9c14f23ea9b1a57f4fee1477fa3c065c
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-12 15:58:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded',function() {
 document.getElementById('myform').addEventListener('submit',function(event){
 //your JS code here. If required.
 event.preventDefault();
-const firstName=document.getElementById('firstName').value;
-const lastName=document.getElementById('lastName').value;
-const phoneNumber=document.getElementById('phoneNumber').value;
-const emailID=document.getElementById('emailID').value;
+const firstName=document.qurySelector('firstName').value;
+const lastName=document.querySelector('lastName').value;
+const phoneNumber=document.querySelector('phoneNumber').value;
+const emailID=document.querySelector('emailID').value;
 	
 const formlogin=`First Name:${firstName}\n Last Name:${lastName}\nPhone Number:${phonenumber}\n EmailID${emailID}`;
 		alert(formlogin);
