c42bf294ef1bbe69514afcf632bfa29ca6135312
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-12 16:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded',function() {
-document.getElementById('myform').addEventListener('submit',function(event){
-//your JS code here. If required.
-event.preventDefault();
-const firstName=document.getElementById('firstName').value;
-const lastName=document.getElementById('lastName').value;
-const phoneNumber=document.getElementById('phoneNumber').value;
-const emailID=document.getElementById('emailID').value;
-	
-const formlogin=`First Name:${firstName}\n Last Name:${lastName}\n Phone Number:${phoneNumber}\n EmailID${emailID}`;
-		alert(formlogin);
-});		
-});
+    document.getElementById('myform').addEventListener('submit',function(event){
+    //your JS code here. If required.
+    event.preventDefault();
+    const firstName=document.getElementById('firstName').value;
+    const lastName=document.getElementById('lastName').value;
+    const phoneNumber=document.getElementById('phonenumber').value;
+    const emailID=document.getElementById('emailID').value;
+        
+    const formlogin=`First Name:${firstName}\n Last Name:${lastName}\n Phone Number:${phoneNumber}\n EmailID: ${emailID}`;
+            alert(formlogin);
+    });		
+    });
